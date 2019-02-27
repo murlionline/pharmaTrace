06950d70a79b1618e7f1c5fe724ab69a54e2b523
Murali Shanmugham <murlionline@gmail.com>
2019-02-27 21:52:52 +1100

    Add files via upload

--- PharmaTrace00/webapp/controller/lookup.controller.js
+++ PharmaTrace00/webapp/controller/lookup.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"sap/m/Dialog"
 ], function (Controller, MessageToast, Dialog) {
 	"use strict";
-
+	
 	var accessToken = "";
 	var ml_clientId = "sb-e56c1387-f909-43af-b3f9-27b701bde488!b11004|ml-foundation-xsuaa-std!b540";
 	var ml_clientSecret = "QH7mvEkCF+49My230B6GCMdtjvw=";
@@ -13,11 +13,14 @@ sap.ui.define([
 	var form = new FormData();
 	var productMaster = {};
 	var scanImageResult,drugName,gtin,manufacturerName,serialNo,batchNo;
-
+	
 	return Controller.extend("com.sap.PharmaTrace00.controller.lookup", {
-
 		onInit: function () {
 			this.getView().byId("fileUploader").addStyleClass("fileUploaderStyle1");
+			
+			var oModel1 = new sap.ui.model.json.JSONModel();
+			oModel1.setDefaultBindingMode("TwoWay");
+			this.getView().setModel(oModel1);
 		},
 
 		handleValueChange: function (oEvent) {
@@ -27,14 +30,17 @@ sap.ui.define([
 
 			var reader = new FileReader();
 			reader.onloadend = function () {
-				var model = oView.getModel().getData();
-				model.image = reader.result;
-				oView.getModel().refresh();
+				// var model = oView.getModel().getData();
+				// model.image = reader.result;
+				// oView.getModel().refresh();
+				
 				oView.byId("fileUploader").addStyleClass("fileUploaderStyle2");
 				oView.byId("uploadBox").setJustifyContent(null);
 				oView.byId("flexBoxHint").setVisible(false);
 				oView.byId("uploadBox").addStyleClass("workListBox2");
 				oView.byId("vBoxImage").setVisible(true);
+				
+				oView.getModel().setProperty("/image", reader.result);
 			};
 
 			reader.readAsDataURL(oEvent.getParameters().files[0]);
@@ -42,210 +48,275 @@ sap.ui.define([
 		},
 
 		callAPI: function (file, oView, oBusyIndicator) {
-
+			oBusyIndicator.open();
 			//var form = new FormData();
 			form.append("files", file);
 			var that = this;
-			this.invokeMLService(file, oView, oBusyIndicator, that);
-			this.invokeHANAService(file, oView, oBusyIndicator, that);
-			this.invokeHyperLedgerService(file, oView, oBusyIndicator, that);
+			this.invokeMLService(file, oView, oBusyIndicator, that).then(
+				function(data){
+					that.invokeHANAService(file, oView, oBusyIndicator, that).then(
+						function(){
+							that.invokeHyperLedgerService(file, oView, oBusyIndicator, that).then(
+								function(){
+									oBusyIndicator.close();
+								});
+						}
+					);
+				}
+			);
 
 		},
 
 		invokeMLService: function (file, oView, oBusyIndicator, that) {
-		
-			// Obtain access token for ML Service
-			$.ajax({
-				url: "/mltoken?grant_type=client_credentials",
-				type: "GET",
-				contentType: "application/json",
-				dataType: "json",
-				async: false,
-				beforeSend: function (xhr) {
-					xhr.setRequestHeader("Authorization", "Basic " + btoa(ml_clientId + ":" + ml_clientSecret));
-				},
-				success: function (response) {
-					accessToken = response.access_token;
-				}
-			});
-
-			// Invoke ML OCR service API using access token to scan label
-			$.ajax({
-				url: /mlservice/,
-				type: "POST",
-				beforeSend: function (xhr) {
-					xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-				},
-				data: form,
-				async: false,
-				processData: false,
-				contentType: false,
-				success: function (data) {
-					try {
-						scanImageResult = data.predictions[0];
-						scanImageResult = scanImageResult.split("\n");
-						manufacturerName = scanImageResult[0];
-						for (var i = 0; i < 12; i++) {
-							if ((scanImageResult[i].substr(0, 7)) == "Product") {
-								drugName = scanImageResult[i].substr(9);
-							}
-							if ((scanImageResult[i].substr(0, 4)) == "GTIN") {
-								gtin = scanImageResult[i].substr(5);
-							}
-							if ((scanImageResult[i].substr(0, 4)) == "SSCC") {
-								serialNo = scanImageResult[i].substr(5);
-							}
-							if ((scanImageResult[i].substr(0, 5)) == "Batch") {
-								batchNo = scanImageResult[i].substr(6);
-							}
+			
+			return new Promise(function(mainResolve, mainReject){
+				
+				var getMLAccessToken = new Promise(function(resolve, reject) {
+				// Obtain access token for ML Service
+					$.ajax({
+						url: "/mltoken?grant_type=client_credentials",
+						type: "GET",
+						contentType: "application/json",
+						dataType: "json",
+						async: true,
+						beforeSend: function (xhr) {
+							xhr.setRequestHeader("Authorization", "Basic " + btoa(ml_clientId + ":" + ml_clientSecret));
+						},
+						success: function (response) {
+							resolve(response.access_token);
+							//accessToken = response.access_token;
 						}
-					} catch (err) {
-						MessageToast.show("Caught - [ajax error] :" + err.message);
+					});
+				});
+				
+				
+				var invokeMLOCRService = function(sToken){
+					return new Promise(function(resolve, reject) {
+						// Invoke ML OCR service API using access token to scan label
+						$.ajax({
+							url: /mlservice/,
+							type: "POST",
+							beforeSend: function (xhr) {
+								xhr.setRequestHeader("Authorization", "Bearer " + sToken);
+							},
+							data: form,
+							async: false,
+							processData: false,
+							contentType: false,
+							success: function (data) {
+								resolve(data);
+							},
+							error: function (request, status, error) {
+								MessageToast.show("Caught - [ajax error] :" + request.responseText);
+							}
+						});
+					});
+				};
+				
+				getMLAccessToken.then(
+					function(data){
+						accessToken = data;
+						invokeMLOCRService(data).then(
+							function(data2){
+								try {
+									scanImageResult = data2.predictions[0];
+									scanImageResult = scanImageResult.split("\n");
+									manufacturerName = scanImageResult[0];
+									for (var i = 0; i < 12; i++) {
+										if ((scanImageResult[i].substr(0, 7)) == "Product") {
+											drugName = scanImageResult[i].substr(9);
+										}
+										if ((scanImageResult[i].substr(0, 4)) == "GTIN") {
+											gtin = scanImageResult[i].substr(5);
+										}
+										if ((scanImageResult[i].substr(0, 4)) == "SSCC") {
+											serialNo = scanImageResult[i].substr(5);
+										}
+										if ((scanImageResult[i].substr(0, 5)) == "Batch") {
+											batchNo = scanImageResult[i].substr(6);
+										}
+										
+										mainResolve(true);
+									}
+								} catch (err) {
+									MessageToast.show("Caught - [ajax error] :" + err.message);
+									mainReject(err);
+								}
+							}
+						);
 					}
-				},
-				error: function (request, status, error) {
-					MessageToast.show("Caught - [ajax error] :" + request.responseText);
-				}
+				);
 			});
 		},
 
 		invokeHANAService: function (file, oView, oBusyIndicator, that) {
 			
-			// Invoke the HANA service API to obtain master data of scanned product
-			$.ajax({
-				url: "/hanaservice/" + "Drugs('" + gtin.trim() + "')",
-				type: "GET",
-				async: false,
-				contentType: "application/json",
-				dataType: "json",
-				success: function (response) {
-					if (response.d) {
-						productMaster.Id = response.d.GTIN;
-						productMaster.name = response.d.name;
-						productMaster.supplierName = response.d.supplier;
-						productMaster.category = response.d.category;
-						productMaster.unitPrice = response.d.unitPrice;
-						productMaster.unitsInStock = response.d.unitInStock;
-						
-						var oView = that.getView();
-
-						oView.byId("manufacturerID").setText(manufacturerName);
-						oView.byId("gtin").setText(gtin);
-						oView.byId("drugName").setText(drugName);
-						oView.byId("productID").setText(productMaster.Id);
-						oView.byId("productName").setText(productMaster.name);
-						oView.byId("supplierName").setText(productMaster.supplierName);
-						oView.byId("category").setText(productMaster.category);
-						oView.byId("unitPrice").setText(productMaster.unitPrice);
-						oView.byId("unitsInStock").setText(productMaster.unitsInStock);
-					}
-				},
-			});
+			return new Promise(
+				function(mainResolve, mainReject){
+					// Invoke the HANA service API to obtain master data of scanned product
+					$.ajax({
+						url: "/hanaservice/" + "Drugs('" + gtin.trim() + "')",
+						type: "GET",
+						async: true,
+						contentType: "application/json",
+						dataType: "json",
+						success: function (response) {
+							if (response.d) {
+								productMaster.Id = response.d.GTIN;
+								productMaster.name = response.d.name;
+								productMaster.supplierName = response.d.supplier;
+								productMaster.category = response.d.category;
+								productMaster.unitPrice = response.d.unitPrice;
+								productMaster.unitsInStock = response.d.unitInStock;
+								
+								//var oView = that.getView();
+		
+								oView.byId("manufacturerID").setText(manufacturerName);
+								oView.byId("gtin").setText(gtin);
+								oView.byId("drugName").setText(drugName);
+								oView.byId("productID").setText(productMaster.Id);
+								oView.byId("productName").setText(productMaster.name);
+								oView.byId("supplierName").setText(productMaster.supplierName);
+								oView.byId("category").setText(productMaster.category);
+								oView.byId("unitPrice").setText(productMaster.unitPrice);
+								oView.byId("unitsInStock").setText(productMaster.unitsInStock);
+								
+								mainResolve(true);
+							}
+						},
+					});
+				}
+			);
 		},
 
 		invokeHyperLedgerService: function (file, oView, oBusyIndicator, that) {
-			
-			// Obtain access token for the Blockchain service
-			$.ajax({
-				url: "/blockchaintoken?grant_type=client_credentials",
-				type: "GET",
-				contentType: "application/json",
-				dataType: "json",
-				async: false,
-				beforeSend: function (xhr) {
-					xhr.setRequestHeader("Authorization", "Basic " + btoa(bs_clientId + ":" + bs_clientSecret));
-				},
-				success: function (response) {
-					accessToken = response.access_token;
-				},
-				timeout: 5000
-			});
-
-			//Generate random unique number for the transaction
-			var transactionId = Math.floor(Math.random() * 99999999);
-			var currentDate = new Date(Date.now()).toLocaleString().split(',')[0];
-			
-			oView.byId("manufacturerID").setText(manufacturerName);
-			oView.byId("gtin").setText(gtin);
-			oView.byId("drugName").setText(drugName);
-			
-			//Construct the payload
-			var payload = {
-				"ID": transactionId,
-				"assetType": "Asset.Serial",
-				"manufacturer": manufacturerName, //remove Id
-				"batchNo": batchNo,
-				"serialNo": serialNo,
-				"manufacturingDate": currentDate,
-				"scanNo": "01",
-				"scanBy": "Manufacturer",
-				"alias": drugName
-				//"description": drugName
-			};
-			
-			//Invoke the Hyperledger fabric API to update a record in the blockchain network
-			$.ajax({
-				url: "/blockchainservice",
-				type: "POST",
-				beforeSend: function (xhr) {
-					xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
-				},
-				contentType: "application/json",
-				dataType: "text",
-				processData: false,
-				data: JSON.stringify(payload),
-				async: false,
-				success: function (data) {
-					try {
-						MessageToast.show("Record has been updated in Blockchain successfully");
-					    //Retrive the newly added transaction
+			return new Promise(
+				function(mainResolve, mainReject){
+					
+					var getHLToken = new Promise(function(resolve, reject) {
+						// Obtain access token for the Blockchain service
 						$.ajax({
-							url: "/blockchainservice/" + transactionId,
+							url: "/blockchaintoken?grant_type=client_credentials",
 							type: "GET",
+							contentType: "application/json",
+							dataType: "json",
+							async: true,
 							beforeSend: function (xhr) {
-								xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+								xhr.setRequestHeader("Authorization", "Basic " + btoa(bs_clientId + ":" + bs_clientSecret));
 							},
-							async: false,
-							success: function (data) {
-								try {
-									var results = data;
-									var p1 = {
-										reviews: []
-									};
-
-									p1.reviews.push({
-										"manufacturer": results.manufacturerId,
-										"pname": results.alias,
-										"batchno": results.batchNo,
-										"scanno": results.scanNo,
-										"serialno": results.serialNo,
-									});
-
-									var oModel1 = new sap.ui.model.json.JSONModel(p1);
-									oModel1.setDefaultBindingMode("TwoWay");
-									that.getView().setModel(oModel1);
-
-								} catch (err) {
-									oBusyIndicator.close();
-									MessageToast.show("Caught - [ajax error] :" + err.message);
-								}
+							success: function (response) {
+								resolve(response.access_token);
 							},
-							error: function (request, status, error) {
-								oBusyIndicator.close();
-								MessageToast.show("Caught - [ajax error] :" + request.responseText);
-							}
+							// timeout: 5000
 						});
-
-					} catch (err) {
-						oBusyIndicator.close();
-						MessageToast.show("Caught - [ajax error] :" + err.message);
-					}
-				},
-				error: function (request, status, error) {
-					oBusyIndicator.close();
-					MessageToast.show("Caught - [ajax error] :" + request.responseText);
+					});
+					
+					var callHLServices = function(accessToken){
+						return new Promise(
+							function(resolve2, reject2){
+								//Generate random unique number for the transaction
+								var transactionId = Math.floor(Math.random() * 99999999);
+								var currentDate = new Date(Date.now()).toLocaleString().split(',')[0];
+								
+								oView.byId("manufacturerID").setText(manufacturerName);
+								oView.byId("gtin").setText(gtin);
+								oView.byId("drugName").setText(drugName);
+								
+								//Construct the payload
+								var payload = {
+									"ID": transactionId,
+									"assetType": "Asset.Serial",
+									"manufacturerId": manufacturerName,
+									"batchNo": batchNo,
+									"serialNo": serialNo,
+									"manufacturingDate": currentDate,
+									"scanNo": "01",
+									"scanBy": "Manufacturer",
+									"alias": drugName,
+									"description": drugName
+								};
+								
+								//Invoke the Hyperledger fabric API to update a record in the blockchain network
+								$.ajax({
+									url: "/blockchainservice",
+									type: "POST",
+									beforeSend: function (xhr) {
+										xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+									},
+									contentType: "application/json",
+									dataType: "text",
+									processData: false,
+									data: JSON.stringify(payload),
+									async: false,
+									success: function (data) {
+										try {
+											MessageToast.show("Record has been updated in Blockchain successfully");
+										    //Retrive the newly added transaction
+											$.ajax({
+												url: "/blockchainservice/" + transactionId,
+												type: "GET",
+												beforeSend: function (xhr) {
+													xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+												},
+												async: true,
+												success: function (data) {
+													resolve2(data);
+												},
+												error: function (request, status, error) {
+													oBusyIndicator.close();
+													MessageToast.show("Caught - [ajax error] :" + request.responseText);
+													reject2(request.responseText);
+												}
+											});
+					
+										} catch (err) {
+											oBusyIndicator.close();
+											MessageToast.show("Caught - [ajax error] :" + err.message);
+										}
+									},
+									error: function (request, status, error) {
+										oBusyIndicator.close();
+										MessageToast.show("Caught - [ajax error] :" + request.responseText);
+										reject2(request.responseText);
+									}
+								});
+							}
+						); 
+					};
+					
+					getHLToken.then(
+						function(data){
+							callHLServices(data).then(
+								function(data2){
+									try {
+										var results = data2;
+										var p1 = {
+											reviews: []
+										};
+	
+										p1.reviews.push({
+											"manufacturer": results.manufacturerId,
+											"pname": results.alias,
+											"batchno": results.batchNo,
+											"scanno": results.scanNo,
+											"serialno": results.serialNo,
+										});
+	
+										that.getView().getModel().setProperty("/reviews", p1.reviews);
+										mainResolve(true);
+	
+									} catch (err) {
+										oBusyIndicator.close();
+										MessageToast.show("Caught - [ajax error] :" + err.message);
+										mainReject(err.message);
+									}
+								}
+							);
+						}
+					);
 				}
-			});
+			);
+			
 		},
 
 		handleRouteMatched: function (oEvent) {
@@ -262,7 +333,6 @@ sap.ui.define([
 					this.getView().bindObject(oPath);
 				}
 			}
-		},
-
+		}
 	});
 });
